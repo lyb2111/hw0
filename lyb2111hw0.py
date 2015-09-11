d4132729b148b04d853b1165335fb80ca35bc406
Ludovico Bassetti <ludovicobassetti@dyn-129-236-237-178.dyn.columbia.edu>
2015-09-11 11:48:56 -0400

    Final hw0 Code

--- lyb2111hw0.py
+++ lyb2111hw0.py
@@ -1,1 +1,7 @@
-Test
+with open ('iowa-liquor-sample.csv') as liquors:
+	counter = 0
+	for line in liquors:
+		lowerline = line.lower()
+		if lowerline.find ("single malt scotch") != -1:
+			counter += 1
+	print (counter)
